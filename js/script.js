a12e49082f7112f1e564a8cba97868f411ca5097
Valer0n <valera.zakrevskyi@gmail.com>
2022-01-08 14:05:22 +0200

    add js and modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const HEADER_MENU = document.querySelector('.header__menu');
     const BUTTON_MENU = HEADER_MENU.querySelector('.button-menu');
-    console.log(BUTTON_MENU);
+    const BUTTON_SCHEME = document.querySelector('.button-scheme');
+    const MODAL = document.querySelector('.modal');
+    const BUTTON_CLOSE_MODAL = MODAL.querySelector('.close');
+
     BUTTON_MENU.addEventListener('click', function() {
         HEADER_MENU.classList.toggle('header__menu--active');
     })
+
+    BUTTON_SCHEME.addEventListener('click', function() {
+        MODAL.classList.toggle('modal--open');
+    })
+
+    BUTTON_CLOSE_MODAL.addEventListener('click', function() {
+        MODAL.classList.remove('modal--open');
+    })
+
+
 });
